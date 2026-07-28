86d6ac45ffb927cf49caf0c16a4b93cf0034f708
Andres Cuervo <andresjimenez.c55@gmail.com>
2026-07-28 11:58:42 -0500

    Componenetes Creados

--- fundamentosreact/src/App.jsx
+++ fundamentosreact/src/App.jsx
@@ -1,48 +1,23 @@
 import './App.css'
 import picture from './assets/rt.png'
-
+import Header from "./components/header"
+import Dbasicos from './components/perfil'
+import Cursos from './components/cursos'
+import Footer from "./components/footer";
 function App() {
   return (
-    <>
-      <div className="card">
-        <img
-          src={picture}s
-          className="foto"
-        />
 
-        <h1>Andres Felipe</h1>
-        <h1>Cuervo Jimenez</h1>      
+      <div className='contenedor'>
 
-        <h2>ANÁLISIS Y DESARROLLO DE SOFTWARE</h2>
-        <h3>Tecnólogo</h3>
-
-        <hr />
-
-        <h2>Hoja de vida del Aprendiz</h2>
-        <p>
-          Estudiante de Análisis y Desarrollo de Software con conocimientos en
-          desarrollo web utilizando HTML, CSS, JavaScript . Me
-          caracterizo por el trabajo en equipo, la responsabilidad y el deseo
-          de aprender nuevas tecnologías.
-        </p>
-
-        <h2>Información Personal</h2>
-        <p><strong>Correo:</strong> andresjimenez.c556@email.com</p>
-        <p><strong>Teléfono:</strong> 3204864084</p>
-        <p><strong>Ciudad:</strong> Bogotá, Colombia</p>
-
-        <h2>Cursos Realizados</h2>
-        <ul>
-          <li>Fundamentos en Data science</li>
-          <li>html and css</li>
-          <li>JavaScript</li>
-          <li>Git y GitHub</li>
-        </ul>
-
-        <button>Descargar Hoja de Vida</button>
+        <Header/>
+        <Dbasicos/>
+        <Cursos/>
+        <Footer/>
       </div>
-    </>
-  )
+
+
+      
+  );
 }
 
 export default App
---
+++ fundamentosreact/src/components/header.jsx
@@ -0,0 +1,10 @@
+function Header()
+{
+    return(
+    <header>
+        <h1>WELCOME TO ?</h1>
+    </header>
+)
+}
+
+export default Header
